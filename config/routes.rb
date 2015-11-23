e64b77e26ee58efdf781931c0304cc35409a19b4
Dalal Alrayes <dalrayes@gmail.com>
2015-11-23 14:23:22 -0600

    renames flower routes and adds view files

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'high_voltage/pages#show', id: 'landing'
-  resources :flowers, only: [:index, :show]
+  resources :arrangements, only: [:index, :show]
   get 'contact' => 'high_voltage/pages#show', id: 'contact'
+  get 'flowers' => 'arrangements#index'
 end
