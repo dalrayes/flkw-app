ff313dc86cfda5d154c20a85d7cdfb38c0b02870
Dalal Alrayes <dalrayes@gmail.com>
2015-11-23 23:15:56 -0600

    Adds instagram feed

---
+++ config/initializers/instagram.rb
@@ -0,0 +1,5 @@
+require 'instagram'
+  Instagram.configure do |config|
+    config.client_id ="39a0252d30d64cf39ea5fc8678140fa6"
+    config.access_token ="8317555.39a0252.730d66f7a4be44f5b00ab28907a691ef"
+ 	end
